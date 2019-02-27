d7c7549f2d68f479bd04282dd17e58b95c96d86a
Ewan Burns <ewan@beezup.com>
2019-02-27 16:18:53 +0100

    Test forcing exit

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -8,5 +8,9 @@ let runtimeFactory = new RuntimeFactory();
 runtimeFactory.CreateRuntimeAsync().then(async (runtime: runtimes.core.IRuntime) => {
   if (runtime) {
     await runtime.RunAsync();
+    //  SCK: REMOVE THIS
+    console.log("REMOVE THIS: Checking this is reached");
+    //  SCK: Test forcing exit
+    process.exit(0);
   }
 });
